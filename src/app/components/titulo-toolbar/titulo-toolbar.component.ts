d1fb926bda4049df27cd5ddaccb0667250538b7d
DESKTOP-FTRLTC2\tulio <matiastulio@gmail.com>
2022-12-17 02:30:48 -0300

    implement PWA functionality

--- src/app/components/titulo-toolbar/titulo-toolbar.component.ts
+++ src/app/components/titulo-toolbar/titulo-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { CrudSeriesComponent } from '../crud-series/crud-series.component';
 
@@ -7,10 +7,21 @@ import { CrudSeriesComponent } from '../crud-series/crud-series.component';
   templateUrl: './titulo-toolbar.component.html',
   styleUrls: ['./titulo-toolbar.component.scss']
 })
-export class TituloToolbarComponent {
+export class TituloToolbarComponent implements OnInit  {
   titulo = 'CadastrarFromApp';
+  isOnline: boolean = false;
 
   constructor(public dialog: MatDialog) {}
+  ngOnInit(): void {
+    this.updateOnlineStatus();
+    window.addEventListener('online',  this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+  }
+
   openDialog(){
     const dialogRef = this.dialog.open(CrudSeriesComponent, {
       width: '470px',
